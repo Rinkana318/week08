85544c6ab79058d00535128e8165d66fcd705046
Max Taylor <mtaylor31801@gmail.com>
2018-10-18 20:37:38 +0000

    first commit

---
+++ content.php
@@ -0,0 +1,44 @@
+<?php 
+
+$allContent = array(
+    "home"=>array(
+        "title"=>"Home",
+        "content"=>"This is the content."
+        ),
+    "about"=>array(
+        "title"=>"About",
+        "content"=>"About fdsjklhdfljh fdkjh sffsad."
+        ),
+    "buy"=>array(
+        "title"=>"Buy Now!",
+        "content"=>"Buy now dsfkhsdfkjhkljhs jhsdfljh sdfj df"
+        ),
+    "contact"=>array(
+        "title"=>"Contact",
+        "content"=>"Contact sdfkjhkljhs jhsdfljh sdfj df"
+        )
+    );
+
+
+$myNav = array(
+    array(
+        "title"=>"Home",
+        "page"=>"index"
+        ),
+    array(
+        "title"=>"Contact",
+        "page"=>"contact"
+        ),
+    array(
+        "title"=>"About",
+        "page"=>"about"
+        ),
+    array(
+        "title"=>"Buy Now!",
+        "page"=>"buy"
+        )
+    );
+    
+$myNavCount = count($myNav);
+
+?>
---
+++ nav.php
@@ -0,0 +1,9 @@
+<?php
+require_once("content.php");
+
+echo  "<ul class='nav'>";
+for($i=0; $i<$myNavCount;$i++){
+echo    "<li class='nav-item'><a class='nav-link' href='".$myNav[$i]['page']."'>".$myNav[$i]['title']."</a></li>";
+}
+echo "</ul>";
+?>
